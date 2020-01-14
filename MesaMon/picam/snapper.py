26d3d799c778fc6720486ad135ca9232e3dd21e6
Ryan T. Hamilton <astrobokonon@gmail.com>
2020-01-14 16:21:41 -0700

    Moving changes over from Ultimon

--- MesaMon/picam/snapper.py
+++ MesaMon/picam/snapper.py
@@ -12,7 +12,8 @@ import numpy as np
 import picamera
 from picamera import PiCamera, Color
 
-import utils, utils_logs
+import utils
+import utils_logs
 
 
 def camInit():
@@ -45,37 +46,51 @@ def camInit():
     return camera
 
 
-def capture(outloc, debug=False):
+def capture(outloc, debug=False, retries=10):
     now = dt.utcnow()
     nowstr = now.strftime("%Y%m%d_%H%M%S")
     print("Starting capture at %s" % (nowstr))
 
-    # Init the camera
-    camera = camInit()
+    # Init the camera. Try a few times if it's busy
+    retryCounter = 0
+    intervalRetries = 10
+    camera = None
 
-    outname = "./%s/%s.png" % (outloc, nowstr)
+    # This allows for a number of retries, in case another process
+    #   is using the camera and isn't immediately available.
+    while camera is None and retryCounter < retries:
+        try:
+            camera = camInit()
+        except picamera.exc.PiCameraMMALError:
+            print("Camera is likely busy! Try again later.")
+            retryCounter += 1
+            print("%d retries remain." % (retries - retryCounter))
+            sleep(intervalRetries)
 
-    # If camera.shutter_speed is 0, camera.exposure_speed will be the 
-    #   actual/used value determined during the above sleeps
-    print("exp: %f, shut: %f" % (camera.shutter_speed, camera.exposure_speed))
-    expspeed = np.round(camera.exposure_speed/1e6, 6)
+    if camera is not None:
+        outname = "./%s/%s.png" % (outloc, nowstr)
 
-    annotation = "Time: %s\nShutterSpeed: %s sec" %  (nowstr, str(expspeed))
+        # If camera.shutter_speed is 0, camera.exposure_speed will be the
+        #   actual/used value determined during the above sleeps
+        print("exp: %f, shut: %f" % (camera.shutter_speed,
+                                     camera.exposure_speed))
+        expspeed = np.round(camera.exposure_speed/1e6, 6)
 
-    # This has to happen *before* the capture!
-    camera.annotate_background = Color("black")
-    camera.annotate_text_size = 18
-    camera.annotate_text = annotation
+        annotation = "Time: %s\nShutterSpeed: %s sec" % (nowstr, str(expspeed))
 
-    # Actually do the capture now!
-    print("Starting capture...")
-    camera.capture(outname)
-    print("Capture complete!")
+        # This has to happen *before* the capture!
+        camera.annotate_background = Color("black")
+        camera.annotate_text_size = 18
+        camera.annotate_text = annotation
 
-    if debug is True:
-        print("Captured %s" % (outname))
+        # Actually do the capture now!
+        print("Starting capture...")
+        camera.capture(outname)
+        print("Capture complete!")
 
-    print("Took a %d microseconds exposure." % (camera.exposure_speed))
+        if debug is True:
+            print("Captured %s" % (outname))
+            print("Took a %d microseconds exposure." % (camera.exposure_speed))
 
     # https://github.com/waveform80/picamera/issues/528
     camera.framerate = 1
@@ -120,4 +135,3 @@ if __name__ == "__main__":
             if (i + 1) % 5 == 0:
                 print(".")
             i += 1
-
