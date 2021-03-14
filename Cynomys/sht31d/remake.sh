b07834d49c458f2634ce22beac06340cedfafb3b
Ryan T. Hamilton <astrobokonon@gmail.com>
2021-03-13 21:08:21 -0700

    Helpful mpy-cross script
    Required on the ESP8266 and the Adafruit SHT31D lib

---
+++ Cynomys/sht31d/remake.sh
@@ -0,0 +1,6 @@
+~/miniconda3/lib/python3.8/site-packages/mpy_cross/mpy-cross main.py
+
+~/miniconda3/lib/python3.8/site-packages/mpy_cross/mpy-cross ../utils.py
+~/miniconda3/lib/python3.8/site-packages/mpy_cross/mpy-cross ../utils_wifi.py
+~/miniconda3/lib/python3.8/site-packages/mpy_cross/mpy-cross adafruit_sht31d_micropy.py
+
